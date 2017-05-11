7e7573fd631e606402b9268f2ba32eb20c15269f
Thaise Dantas | Stone <tdantas@stone.com.br>
2017-05-11 16:03:08 -0300

    upload project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RabbitMQService rabbitMQService = new RabbitMQService();
+            IConnection connection = rabbitMQService.GetRabbitMQConnection();
+            IModel model = connection.CreateModel();
+
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.SetPersistent(false);
+
+            //enviando a mensagem no formato de matriz de bytes
+            byte[] payload = Encoding.UTF8.GetBytes("This message from Visual Studio");
+
+            //construindo o endereço
+            PublicationAddress address = new PublicationAddress(ExchangeType.Topic, "exchangeFromVisualStudio", "superstar");
+
+            //enviando a mensagem
+            model.BasicPublish(address, basicProperties, payload);
+
+        }
+        private static void SetupInitialTopicQueue(IModel model)
+        {
+            //criando a fila
+            model.QueueDeclare("queueFromVisualStudio", true, false, false, null);
+            //criando a troca
+            model.ExchangeDeclare("exchangeFromVisualStudio", ExchangeType.Topic);
+            //ligando a fila a troca
+            model.QueueBind("queueFromVisualStudio", "exchangeFromVisualStudio", "superstar");
+        }
+
+
+
+    }
+}
---
+++ HelloWorld/RabbitMQService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace HelloWorld
+{
+    public class RabbitMQService
+    {
+        public IConnection GetRabbitMQConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = "localhost";
+            connectionFactory.UserName = "guest";
+            connectionFactory.Password = "guest";
+
+            return connectionFactory.CreateConnection();
+        }
+
+        
+
+    }
+}
